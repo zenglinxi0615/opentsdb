dfa3a0b0563aacd041b685cfd780efd4b214c846
zenglinxi0615 <zenglinxi0615@163.com>
2013-07-08 23:50:57 +0800

    Update Plot.java
    support stacked graph like the memory mornitor graph in ganglia, and show the statistical information of all the datapoints in titles.

--- src/graph/Plot.java
+++ src/graph/Plot.java
@@ -14,10 +14,20 @@ package net.opentsdb.graph;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Iterator;
+import java.util.TreeMap;
+import java.lang.Double;
+
 import java.util.TimeZone;
 
 import org.slf4j.Logger;
@@ -26,6 +36,8 @@ import org.slf4j.LoggerFactory;
 import net.opentsdb.core.DataPoint;
 import net.opentsdb.core.DataPoints;
 import net.opentsdb.meta.Annotation;
+
+import java.math.BigDecimal;
 
 /**
  * Produces files to generate graphs with Gnuplot.
@@ -55,7 +67,7 @@ public final class Plot {
 
   /** List of global annotations */
   private List<Annotation> globals = null;
-  
+
   /** Per-DataPoints Gnuplot options. */
   private ArrayList<String> options = new ArrayList<String>();
 
@@ -77,6 +89,20 @@ public final class Plot {
    * to get local time.
    */
   private final short utc_offset;
+ 
+  private Map<String, List<String>> query = null;
+  
+  /** max value of all the datapoints. */
+  private double max = 0;
+
+  /** min value of all the datapoints. */
+  private double min = 0;
+
+  /** current value of the datapoints. */
+  private double cur = 0;
+
+  /** average value of all the datapoints. */
+  private double avg = 0;
 
   /**
    * Constructor.
@@ -155,7 +181,7 @@ public final class Plot {
   public void setGlobals(final List<Annotation> globals) {
     this.globals = globals;
   }
-  
+
   /**
    * Adds some data points to this plot.
    * @param datapoints The data points to plot.
@@ -181,7 +207,188 @@ public final class Plot {
     return datapoints;
   }
 
+  public void setQuery(final Map<String, List<String>> query) {
+    this.query = query;
+  }
+
   /**
+   * Returns a processed result when the value is larger than 1000.
+   * for example: 17600 -> 17.60k.
+   */
+  private String convert(double num) {
+    String result = "";
+    if (num > 1000) {
+      num = round(num / 1000, 2, BigDecimal.ROUND_DOWN);
+      result = num + "k";
+    } else {
+      result += num;
+    }
+    return result;
+  }
+
+  /**
+   * Returns max value of all datapoints, add by zlx.
+   * Do not attempt to modify the return value.
+   */
+  public String getMax() {
+    String max_str = convert(max); 
+    return max_str;
+  }
+
+  /**
+   * Returns min value of all datapoints.
+   * Do not attempt to modify the return value.
+   */
+  public String getMin() {
+    String min_str = convert(min); 
+    return min_str;
+  }
+
+  /**
+   * Returns current value of all datapoints.
+   * Do not attempt to modify the return value.
+   */
+  public String getCur() {
+    String cur_str = convert(cur); 
+    return cur_str;
+  }
+
+  /**
+   * Returns averge value of all datapoints.
+   * Do not attempt to modify the return value.
+   */
+  public String getAvg() {
+    String avg_str = convert(avg);
+    return avg_str;
+  }
+
+  /**
+   * Set the scale of a double value.
+   * To reduce the length of title as we don't need a absolute accuracy value.
+   */
+  private double round(double value, int scale, int roundingMode) {
+    BigDecimal bd = new BigDecimal(value);
+    bd = bd.setScale(scale, roundingMode);
+    double d = bd.doubleValue();
+    bd = null;
+    return d;
+  }
+
+  /**
+   * Get the statistical information of datapoints including max, min, avg and cur value.
+   * @anthor zlx 
+   */
+  public void setStatisticalInfo(DataPoints dp) {
+    int npoints = 0;
+    if (dp.size() <= 0) {
+      return;
+    }
+    double max = -Double.MAX_VALUE;
+    double min = Double.MAX_VALUE;
+    double cur = 0;
+    long ts_cur = 0;
+    double avg = 0;
+    double sum = 0;
+    double value = 0;
+    
+    for (final DataPoint d : dp) {
+      final long ts = d.timestamp();
+      if (ts >= (start_time & UNSIGNED) && ts <= (end_time & UNSIGNED) ) {
+        npoints++;
+        if (d.isInteger()) {
+          value = d.longValue();
+        } else {
+          value = d.doubleValue();
+          if (value != value || Double.isInfinite(value)) {
+            throw new IllegalStateException("NaN or Infinity found in"
+                + " datapoints #" + npoints + ": " + value + " d=" + d);
+          }
+        }
+        sum += value;
+        max = max < value ? value : max;
+        min = min > value ? value : min;
+        cur = ts > ts_cur ? value : cur;
+        ts_cur = ts > ts_cur ? ts : ts_cur;
+     }
+    }
+
+    if (npoints > 0) {
+       avg = sum / npoints;
+    } else {
+       max = 0;
+       min = 0;
+    }
+    
+    this.max = round(max, 2, BigDecimal.ROUND_DOWN);
+    this.min = round(min, 2, BigDecimal.ROUND_DOWN);
+    this.cur = round(cur, 2, BigDecimal.ROUND_DOWN);
+    this.avg = round(avg, 2, BigDecimal.ROUND_DOWN);
+  }      
+
+  /*
+   * merge the data files for transparent stack model to avoid color mixture.
+   * @param basepath The base path to use.
+   * @param datafiles The names of the data files that need to be plotted,
+   * in the order in which they ought to be plotted.  It is assumed that
+   * the ith file will correspond to the ith entry in {@code datapoints}.
+   * Can be {@code null} if there's no data to plot.
+   * @throws IOException if there was an error while writing the file.
+   * @anthor zlx 
+   */
+  public void dataFilesMerge(final String basepath, final String[] datafiles) throws IOException {
+    LOG.info("start merge data file... ");
+    final int nseries = datapoints.size();
+    Map<String, List<String>> data_map = new TreeMap<String, List<String>>(); 
+    for (int i = 0; i < nseries; i++) {
+      LOG.info("reading data file: " + datafiles[i]);
+      FileReader reader = new FileReader(datafiles[i]);
+      BufferedReader br = new BufferedReader(reader);
+      try {
+        String dataline = null;
+        while((dataline = br.readLine()) != null) {
+          String[] splits = dataline.split(" ");
+          String ts = splits[0];
+          String value = splits[1];
+          if (data_map.containsKey(ts)) {
+            List<String> dp_list = data_map.get(ts);
+            dp_list.set(i, value);
+          } else {
+            String[] dp_array = new String[nseries];
+            List<String> dp_list = Arrays.asList(dp_array);
+            dp_list.set(i, value);
+            data_map.put(ts, dp_list);
+          }
+        }
+      } finally {
+        br.close();
+        reader.close();
+      }
+    }
+    final String mergefile_name = basepath + "_mergefile" + ".dat";
+    final FileWriter mergefile = new FileWriter(mergefile_name);
+    BufferedWriter bw = new BufferedWriter(mergefile);
+    Iterator it = data_map.entrySet().iterator();
+    try {
+      while (it.hasNext()) {
+        Map.Entry entry = (Map.Entry) it.next();
+        String key = (String) entry.getKey();
+        List<String> value = (List<String>) entry.getValue();
+        String dataline = key + " ";
+        for (int j = 0; j < nseries; j++) {
+          dataline += value.get(j) + " ";
+        }
+        bw.write(dataline);
+        bw.newLine();
+      }
+    } finally {
+      bw.flush();
+      bw.close();
+      mergefile.close();
+    }
+    LOG.info("finished merge data file, create dat file: " + mergefile_name);
+  }
+
+  /*
    * Generates the Gnuplot script and data files.
    * @param basepath The base path to use.  A number of new files will be
    * created and their names will all start with this string.
@@ -193,33 +400,61 @@ public final class Plot {
   public int dumpToFiles(final String basepath) throws IOException {
     int npoints = 0;
     final int nseries = datapoints.size();
+    //add by zlx, to support stack graphs such as memory graph in ganglia
+    final String stacked = params.get("stacked");
+    final String haveTotal = params.get("haveTotal");
     final String datafiles[] = nseries > 0 ? new String[nseries] : null;
+    int flag = stacked == null ? 0 : 1;
+    if (flag == 1) {
+      LOG.info("stacked model turn on ... ");
+    } else {
+      LOG.info("stacked model off ... ");
+    }
+    Map<String, Double> last_values = new HashMap<String, Double>();
     for (int i = 0; i < nseries; i++) {
+      if (i == nseries - 1 && haveTotal != null) {
+        flag = 0;
+      }
       datafiles[i] = basepath + "_" + i + ".dat";
       final PrintWriter datafile = new PrintWriter(datafiles[i]);
       try {
+        int index = -1;
+        int count = 0;
         for (final DataPoint d : datapoints.get(i)) {
+          index++;
           final long ts = d.timestamp();
           if (ts >= (start_time & UNSIGNED) && ts <= (end_time & UNSIGNED)) {
             npoints++;
           }
           datafile.print(ts + utc_offset);
           datafile.print(' ');
+          double value = 0;
           if (d.isInteger()) {
-            datafile.print(d.longValue());
+            //datafile.print(d.longValue());
+            value = d.longValue();
           } else {
-            final double value = d.doubleValue();
+            value = d.doubleValue();
             if (value != value || Double.isInfinite(value)) {
               throw new IllegalStateException("NaN or Infinity found in"
                   + " datapoints #" + i + ": " + value + " d=" + d);
             }
-            datafile.print(value);
           }
+          double tmp = 0;
+          String timestamp = String.valueOf(ts);
+	        if (last_values.containsKey(timestamp)) {
+            tmp = last_values.get(timestamp);
+	        }
+          value += tmp * flag;
+	        last_values.put(timestamp, new Double(value));
+          datafile.print(value);
           datafile.print('\n');
         }
       } finally {
         datafile.close();
       }
+    }
+    if (stacked != null && nseries > 0) {
+      dataFilesMerge(basepath, datafiles); 
     }
 
     if (npoints == 0) {
@@ -231,6 +466,37 @@ public final class Plot {
     }
     writeGnuplotScript(basepath, datafiles);
     return npoints;
+  }
+
+  /*
+   * read the color config file to set the color for metric,
+   * if the color of a metric is undefined, gnuplot will set a default color.
+   * @return the map which contains metric_name -> color pairs.
+   * @anthor zlx 
+   */
+  private Map<String, String> readColorDefinedFile(String fileName) {
+    Map<String, String> user_defined_color = new HashMap<String, String>();	  
+    try {
+      FileReader reader = new FileReader(fileName);
+      BufferedReader br = new BufferedReader(reader);
+      if (reader != null) {
+        try {
+          String line = null;
+          while((line = br.readLine()) != null) {
+            String[] splits = line.split(" ");
+            String metric_name = splits[0];
+            String color = splits[1];
+            user_defined_color.put(metric_name, color);
+          }
+        } finally {
+          br.close();
+          reader.close();
+        }
+      }
+    } catch (IOException e) {
+      LOG.info(e.toString());
+    } 
+    return user_defined_color;
   }
 
   /**
@@ -245,13 +511,20 @@ public final class Plot {
                                   final String[] datafiles) throws IOException {
     final String script_path = basepath + ".gnuplot";
     final PrintWriter gp = new PrintWriter(script_path);
+    final String font = params.remove("font");
+    final int nseries = datapoints.size();
+    Map<String, String> user_defined_color = null;
+	  user_defined_color = readColorDefinedFile("/home/zenglinxi/user_defined_color");
     try {
       // XXX don't hardcode all those settings.  At least not like that.
-      gp.append("set term png small size ")
+      gp.append("set terminal png small size ")
         // Why the fuck didn't they also add methods for numbers?
         .append(Short.toString(width)).append(",")
-        .append(Short.toString(height));
+	      // modified by zlx for setting font of the graphs
+        .append(Short.toString((short)(height + ((nseries - 1) * 10)))).append(" font \"MSYH.TTF, " + font + "\""); 
       final String smooth = params.remove("smooth");
+      final String stacked = params.remove("stacked");
+      final String haveTotal = params.remove("haveTotal");
       final String fgcolor = params.remove("fgcolor");
       String bgcolor = params.remove("bgcolor");
       if (fgcolor != null && bgcolor == null) {
@@ -272,6 +545,10 @@ public final class Plot {
       }
 
       gp.append("\n"
+                + "set key font \"MSYH.TTF, " + font + " \"\n"
+                + "set key Left\n"
+                + "set key reverse\n"
+                + "set style fill transparent solid 0.5 noborder\n"
                 + "set xdata time\n"
                 + "set timefmt \"%s\"\n"
                 + "if (GPVAL_VERSION < 4.6) set xtics rotate; else set xtics rotate right\n"
@@ -284,7 +561,6 @@ public final class Plot {
       if (!params.containsKey("format x")) {
         gp.append("set format x \"").append(xFormat()).append("\"\n");
       }
-      final int nseries = datapoints.size();
       if (nseries > 0) {
         gp.write("set grid\n"
                  + "set style data linespoints\n");
@@ -317,7 +593,7 @@ public final class Plot {
           break;
         }
       }
-      
+
       // compile annotations to determine if we have any to graph
       final List<Annotation> notes = new ArrayList<Annotation>();
       for (int i = 0; i < nseries; i++) {
@@ -346,26 +622,67 @@ public final class Plot {
       }
 
       gp.write("plot ");
-      for (int i = 0; i < nseries; i++) {
+      for (int i = nseries - 1; i >= 0; i--) {
         final DataPoints dp = datapoints.get(i);
-        final String title = dp.metricName() + dp.getTags();
-        gp.append(" \"").append(datafiles[i]).append("\" using 1:2");
+        setStatisticalInfo(dp);
+        final String title = dp.metricName() + "{Cur: " + getCur() + "  Min: " + getMin() + "  Max: " + getMax() + "  Avg: " + getAvg() + "}";
+        final String mergefile_name = basepath + "_mergefile" + ".dat";
+        if (stacked == null || i == 0) {
+          gp.append(" \"").append(datafiles[i]).append("\" using 1:2");
+        } else if (i == nseries - 1 && haveTotal != null) {
+            gp.append(" \"").append(mergefile_name).append("\" using 1:" + (i + 2));
+        } else {
+            gp.append(" \"").append(mergefile_name).append("\" using 1:" + (i + 1) + ":" + (i + 2));
+        }
         if (smooth != null) {
           gp.append(" smooth ").append(smooth);
         }
-        // TODO(tsuna): Escape double quotes in title.
-        gp.append(" title \"").append(title).write('"');
+        //add by zlx, just for one especial case
+        //some metrics only have two values 0 or 1
+        if (dp.metricName().equals("ping")) {
+          gp.append(" with steps ");
+        }
+        // if we need stacked graph, we should decide which area will be filled.
+        if (stacked != null  && (i != nseries -1 || haveTotal == null)) {
+          if (i == 0) {
+            gp.append(" w filledcurves x1");
+          } else {
+            //this will fill the area between two curves
+            gp.append(" w filledcurves ");
+          }
+     	  // if there are only one curves, we prefer to fill it with green
+          if (nseries == 1) {
+            gp.append(" lc 2 ");
+          } else if (user_defined_color != null && user_defined_color.containsKey(dp.metricName())) {
+              gp.append(" lc " + user_defined_color.get(dp.metricName()) + " ");
+          }
+          gp.append(" lw 2 title \"").append(title).write('"');
+        } else {
+          gp.append(" pt 0 lw 2 title \"").append(title).write('"');
+        }
         final String opts = options.get(i);
         if (!opts.isEmpty()) {
           gp.append(' ').write(opts);
         }
-        if (i != nseries - 1) {
+        //if (i != nseries - 1) {
+        if (i != 0) {
           gp.print(", \\");
         }
         gp.write('\n');
       }
       if (nseries == 0) {
-        gp.write('0');
+        //gp.write('0');
+        //add by zlx, if the graph has no data, we also want to show the titles
+        List<String> metrics = query.get("m");
+        int size = metrics.size();
+        for(int i = 0; i < size; i++) {
+          String metric_name = metrics.get(i).split(":")[2];
+          gp.append(" 0 pt 0 lw 2 title \"").append(metric_name).write('"');
+          if (i != size - 1) {
+            gp.print(", \\");
+          }
+          gp.write('\n');
+        }
       }
     } finally {
       gp.close();
